b27be7030d81cb76ac910b5af8ba317c6a214943
M. Nura Alam Naim <138007943+Nura-Alam-Naim@users.noreply.github.com>
2024-11-18 22:42:32 +0600

    Changed

--- Closest_Pair_Divide_and Conquor/Closest_pair.cpp
+++ Closest_Pair_Divide_and Conquor/Closest_pair.cpp
@@ -13,8 +13,7 @@ typedef vector<pair<ll, ll> > vp;
 #define ff first
 #define ss second
 const ll N = 1e6 + 1;
-ll x1, y, x2, y2;
-double ds;
+vp closed(2);
 double min(double a, double b)
 {
     return (a < b) ? a : b;
@@ -31,55 +30,65 @@ double close(vp v, ll size, double d)
 {
     double mn = d;
     sort(all(v), comp);
-    for (ll i = 0; i < size; ++i)
-        for (ll j = i + 1; j < size and (double)(v[j].ss - v[i].ss) < mn; j++)
+    for (ll i = 0; i < size; i++)
+        for (ll j = i + 1; j < size; j++)
             if (dis(v[i], v[j]) < mn)
+            {
                 mn = dis(v[i], v[j]);
-
+                closed[0] = v[i];
+                closed[1] = v[j];
+            }
+    return mn;
+}
+double brute(vp &v)
+{
+    double mn = DBL_MAX;
+    for (ll i = 0; i < v.size(); i++)
+    {
+        for (ll j = i + 1; j < v.size(); j++)
+        {
+            if (mn > dis(v[i], v[j]))
+            {
+                mn = dis(v[i], v[j]);
+                closed[0] = v[i];
+                closed[1] = v[j];
+            }
+        }
+    }
     return mn;
 }
 double closestPath(vp v, ll l, ll r)
 {
     if(r-l<=2)
-    {
-        double mn = DBL_MAX;
-        for (ll i = l; i < r ; i++)
-            for (ll j = i + 1; j < r; j++)
-                if (dis(v[i], v[j]) < mn)
-                {
-                    mn = dis(v[i], v[j]);
-                    ds = mn;
-                    x1 = v[i].ff;
-                    y = v[i].ss;
-                    x2 = v[j].ff;
-                    y2 = v[j].ss;
-                }
-        return mn;
-    }
+        return brute(v);
     ll mid = (r + l) / 2;
     double d1 = closestPath(v, l, mid);
     double d2 = closestPath(v, mid + 1, r);
     double d = min(d1, d2);
-    ds = d;
     vp strip(r - l + 1);
     ll cnt = 0;
     for (ll i = 0; i <= (r-l); i++)
-        if (abs(v[i].ff - v[mid].ff) < d)
+        if (fabs(v[i].ff - v[mid].ff) < d)
             strip[cnt++] = v[i];
     double d3 = close(strip, cnt,d);
     return min(d3, d);
 }
 void solve()
 {
+    //vp v = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4}};
+    //cout << "Pairs: " << nl;
     vp v(6);
-    v[0] = make_pair(2, 3); v[1] = make_pair(12,30);
-    v[2] = make_pair(40, 50); v[3] = make_pair(5, 1);
-    v[4] = make_pair(12, 10); v[5] = make_pair(3, 4);
-    cout << "Pairs: " << nl;
-    for (auto i: v)
-        cout<< i.ff << sp << i.ss << nl;
+    v[0] = make_pair(2, 3);
+    v[1] = make_pair(12, 30);
+    v[2] = make_pair(40, 50);
+    v[3] = make_pair(5, 1);
+    v[4] = make_pair(12, 10);
+    v[5] = make_pair(3, 4);
     sort(all(v));
-    cout << closestPath(v, 0, 5) << nl;
+    cout << "Closest Distance: "<<fixed<<setprecision(6)<<closestPath(v, 0, 5) << nl;
+    cout << "Closest pairs are (x,y): " << nl;
+    for (auto i : closed)
+        cout << i.ff << sp << i.ss << nl;
 }
 int main()
 {
